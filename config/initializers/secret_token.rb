8c3939fdc069fcb422f605f2df6a68284379bd40
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-12-09 18:24:51 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bard::Application.config.secret_token = '8146ddaf537e619c65911001e1185eb2cf70223c6e7b7b051435899e2b2ac0f376464a2128952c92d5d3c6704fe7f3c7ca7eb1d66fd4358c868b6c4118e0ec9d'
